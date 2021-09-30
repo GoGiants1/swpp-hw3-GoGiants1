869199bd9c44e74daa22c2cf1be5411911955433
GoGiants1 <chooi221@snu.ac.kr>
2021-09-30 16:55:16 +0900

    make custom boiler-plate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureAppStore from './app/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureAppStore();
+const renderApp = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  );
+
+  if (process.env.NODE_ENV !== 'production' && module.hot) {
+    module.hot.accept('./App', renderApp);
+  }
+};
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
