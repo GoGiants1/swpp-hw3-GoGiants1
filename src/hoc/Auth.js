ed6371395a6f270071b3c80ea413bbca5cad2286
GoGiants1 <chooi221@snu.ac.kr>
2021-10-03 03:40:53 +0900

    Modify push logic

--- src/hoc/Auth.js
+++ src/hoc/Auth.js
@@ -1,27 +1,22 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-// import { Redirect } from 'react-router';
-import { push } from 'connected-react-router';
+import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../features/user/userSlice';
 
 export default (Component, option /* { admin = null} */) => {
   /* 
-          option: null -> 아무나 다 접근 가능.
-                  true -> 로그인한 유저만 접근
-                  false -> 로그인 안한 사람들만 접근
+      option ===> null -> 아무나 다 접근 가능, true -> 로그인한 유저만 접근, false -> 로그인 안한 사람들만 접근
   */
 
   const Auth = (props) => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
-    const dispatch = useDispatch();
+    const { location, history } = props;
+    const { pathname } = location;
     useEffect(() => {
       // 로그인 안한 사람.
-      console.log(isLoggedIn);
-      console.log(props);
-
-      if (!isLoggedIn && option) {
-        // <Redirect to="/login" />;
-        dispatch(push('/login'));
+      if (!isLoggedIn && (pathname === '/' || option)) {
+        history.push('/login');
+      } else if (isLoggedIn && pathname === '/') {
+        history.push('/articles');
       }
     }, []);
     return <Component />;
